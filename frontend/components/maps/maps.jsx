8550255b7c173553be756a461404b9e331cf6eb9
Nick <nbarrameda89@gmail.com>
2022-01-12 10:33:53 -0500

    fixed map loading on show page. Added show page to center one single position

--- frontend/components/maps/maps.jsx
+++ frontend/components/maps/maps.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {GiGoblinCamp} from 'react-icons/gi'
+import {FaCampground} from 'react-icons/fa'
 
 class Maps extends React.Component{
     constructor(props){
@@ -10,14 +12,39 @@ class Maps extends React.Component{
 
         // let pos
         // let mark
+        debugger
+        let center
+        if (this.props.spots.length === 1) {
+            center = {lat: this.props.spots[0].latitude, lng: this.props.spots[0].longitude,}
+        } else {
+            center = { lat: 42.06, lng: -73.76242 }
+        }
         
         const mapOptions = {
-            center: { lat: 42.06, lng: -73.76242 },
+            center: center,
             zoom: 8,
-            mapTypeId: 'terrain'
+            mapTypeId: 'terrain',
           };
       
         this.map = new google.maps.Map(this.mapNode, mapOptions);
+
+        const icon = {
+            url: "https://cdn-icons-png.flaticon.com/512/3313/3313260.png",
+            scaledSize: new google.maps.Size(50, 50)
+        };
+        let options = {position: {lat: 42.06, lng: -73.76242}, icon: icon}
+        let mark
+
+        if (this.props.spots.length !== 0) {
+            debugger
+            this.props.spots.forEach(spot => {
+                    options = { position: new google.maps.LatLng(spot.latitude, spot.longitude),
+                                icon: icon },
+                    mark = new google.maps.Marker(options),
+                    mark.setMap(this.map);
+            })
+        }; 
+        
           
         //   if (!this.props.spots === null) {
         //       this.props.spots.each(spot => 
@@ -37,16 +64,22 @@ class Maps extends React.Component{
     render(){
     // debugger
 
-        let pos
+        const icon = {
+            url: "https://cdn-icons-png.flaticon.com/512/3313/3313260.png",
+            scaledSize: new google.maps.Size(50, 50)
+        };
+        let options = {position: {lat: 42.06, lng: -73.76242}, icon: icon}
         let mark
 
         if (this.props.spots.length !== 0) {
             debugger
             this.props.spots.forEach(spot => {
-                    pos = { position: new google.maps.LatLng(spot.latitude, spot.longitude) },
-                    mark = new google.maps.Marker(pos),
+                    options = { position: new google.maps.LatLng(spot.latitude, spot.longitude),
+                                icon: icon },
+                    mark = new google.maps.Marker(options),
                     mark.setMap(this.map);
-            })} 
+            })
+        }; 
             
             // else if (this.props.spot !== null) {
             // pos = { position: new google.maps.LatLng(this.props.spot.latitude, this.props.spot.longitude) },
