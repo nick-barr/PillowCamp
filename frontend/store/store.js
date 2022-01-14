91d8565b2670a25dc77cf93741b7b956edb87cf3
Nick <nbarrameda89@gmail.com>
2022-01-14 09:26:25 -0500

    logger removed from state for heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,7 +5,8 @@ import RootReducer from '../reducers/root_reducer'
 
 const configureStore = (preloadedState = {}) => {
     return(
-        createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+        // createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+        createStore(RootReducer, preloadedState, applyMiddleware(thunk))
         );
 }
 
