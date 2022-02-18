c82880568e9ae351406f5df837cdf249b508419a
Nick <nbarrameda89@gmail.com>
2022-02-18 09:27:16 -0500

    updated all buttons and links to show as cursor pointer

--- frontend/components/reviews/reviews.jsx
+++ frontend/components/reviews/reviews.jsx
@@ -41,30 +41,31 @@ class Reviews extends React.Component{
 
     newReviewInput(){
         if (this.props.userId) {
-        
             return(
-                    <form className='review-form' onSubmit={this.handleCreate}>
-                        <label>Leave a review</label>
-                        <div className='review-form-controls'>
-                            <textarea
-                                id='textarea-review' 
-                                onChange={this.formUpdates("body")} 
-                                placeholder="Tell us your thoughts" 
-                                required>
-                            </textarea>
-                            <button className='review-form__submit'>Submit Review</button>
-                        </div>
-                    </form>
-                )
+                <form className='review-form' onSubmit={this.handleCreate}>
+                    <label>Leave a review</label>
+                    <div className='review-form-controls'>
+                        <textarea
+                            id='textarea-review' 
+                            onChange={this.formUpdates("body")} 
+                            placeholder="Tell us your thoughts" 
+                            required>
+                        </textarea>
+                        <button className='review-form__submit'>Submit Review</button>
+                    </div>
+                </form>
+            )
         }
     }
 
     render(){
+        const { reviews } = this.props;
+
         return(
             <div className='user-review-container'>
                 <div>{this.newReviewInput()}</div>
-                <h1>{this.props.reviews.length} Reviews</h1>
-                {this.props.reviews.map(review =>
+                <h1>{reviews.length} Reviews</h1>
+                {reviews.map(review =>
                 <div className='user-review'>
                     <img src="https://sharespost-sharex-production.s3.us-west-2.amazonaws.com/uploads/issuer/100001872/logo/hipcamp-logo.png" alt="" />
                         <div className='review-content'>
